022b008acf98c855f0f6197a0ef2e3e9d40d4b43
lorena <e.lorenacasfer@go.ugr.es>
2024-01-14 20:11:43 +0100

    añadir productos desde anuncia y poder elimniarlos

--- DepMarketing/urls.py
+++ DepMarketing/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import ListaMarketing,agregar_socio, eliminar_socio, buscar_socio, modificar_socio, mostrar_socio, buscar_anuncio, mostrar_anuncio, agregar_anuncio, eliminar_anuncio, modificar_anuncio
+from .views import ListaMarketing,agregar_socio, eliminar_socio, buscar_socio, modificar_socio, mostrar_socio, buscar_anuncio, mostrar_anuncio, agregar_anuncio, eliminar_anuncio, modificar_anuncio, anuncio_de_producto, eliminar_el_producto 
 
 urlpatterns = [
     path('', ListaMarketing.as_view(), name= 'ListaMarketing'),
@@ -12,5 +12,7 @@ urlpatterns = [
     path('mostrar_anuncio/<str:CodigoA>/', mostrar_anuncio, name='mostrar_anuncio'),
     path('modificar_anuncio/<str:CodigoA>/', modificar_anuncio, name='modificar_anuncio'),
     path('agregar_anuncio', agregar_anuncio, name = 'agregar_anuncio'),
-    path('eliminar_anuncio/<str:CodigoA>/', eliminar_anuncio, name='eliminar_anuncio')
+    path('eliminar_anuncio/<str:CodigoA>/', eliminar_anuncio, name='eliminar_anuncio'),
+    path('insertar_producto/<str:CodigoA>/', anuncio_de_producto, name='insertar_producto'),
+    path('eliminar_el_producto/<str:CodigoA>/<int:Prod>', eliminar_el_producto, name='eliminar_el_producto')
 ]
